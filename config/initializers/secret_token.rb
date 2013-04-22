f04c2c2bd2cd5f95a02f342365337a0130a8fbc9
Emily Durrant <emily.l.durrant@gmail.com>
2013-04-21 20:33:59 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Construction::Application.config.secret_token = '21c33268b016c3ada33ad99c5f81be7f74e2adccf6a4dc249705ff492f70812d1762e986ad76a9664f000cb4bb3d9e215bcb729581722e9738e9bd3c00d3e751'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Construction::Application.config.session_store :cookie_store, key: '_Construction_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Construction::Application.config.session_store :active_record_store
